8d3e0dacdbda2594b2bb72d224ddd63eaa3f4bc4
Trent Bennett <rehab.rtb@gmail.com>
2020-05-01 10:22:55 -0600

    assembler code

---
+++ Assembler/Binary.java
@@ -0,0 +1,132 @@
+package Assembler;
+
+import static java.lang.Math.pow;
+
+public class Binary {
+    private String value = "";
+    private int decValue = 0;
+    private int places = 0;
+
+    public Binary(int num, int _places) {
+        decValue = num;
+        places = _places;
+        value = this.intToBin(num, places);
+    }
+
+    public Binary(String num) {
+        value = num;
+        places = num.length();
+        decValue = this.binToInt(num);
+    }
+
+    public String twosCompliment() {
+        String temp = this.subtract(1);
+        String result = "";
+        int a = 0;
+        for (int i = 0; i < value.length(); i++) {
+            a = temp.charAt(i) - 48;
+            a = (a + 1) % 2;
+            result = result + a;
+        }
+        return result;
+    }
+
+    public String twosCompliment(String str) {
+        value = str;
+        String temp = this.intToBin(((this.getDecimalValue() * -1) - 1), places);
+        String result = "";
+        int a = 0;
+        for (int i = 0; i < value.length(); i++) {
+            a = temp.charAt(i) - 48;
+            a = (a + 1) % 2;
+            result = result + a;
+        }
+        return result;
+    }
+
+    public String add(int num) {
+        decValue += num;
+        value = intToBin(decValue, places);
+        return value;
+    }
+
+    public String add(String num) {
+        int temp = this.binToInt(num);
+        decValue += temp;
+        value = this.intToBin(decValue, places);
+        return value;
+    }
+
+    public String subtract(String num) {
+        int temp = decValue - this.binToInt(num);
+        return this.intToBin(decValue, places);
+    }
+
+    public String subtractSet(String num) {
+        decValue -= this.binToInt(num);
+        value = this.intToBin(decValue, places);
+        return value;
+    }
+
+    public String subtract(int num) {
+        int temp = decValue - num;
+        return this.intToBin(decValue, places);
+    }
+
+    public String subtractSet(int num) {
+        decValue -= num;
+        value = this.intToBin(decValue, places);
+        return value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public int getDecimalValue() {
+        return decValue;
+    }
+
+    private void setValue(int num) {
+        decValue = num;
+        value = this.intToBin(decValue, places);
+    }
+
+    private void setValue(String num) {
+        value = num;
+        decValue = this.binToInt(num);
+    }
+
+    public String intToBin(int num, int places) {
+        String result = "";
+        int temp = num;
+        if (temp < 0) {
+            temp = temp * -1;
+        }
+        while (temp > 0) {
+            int b = temp % 2;
+            result = b + result;
+            temp = temp / 2;
+        }
+        int i = result.length();
+        while (i < places) {
+            result = "0" + result;
+            i++;
+        }
+        if (num < 0) {
+            return this.twosCompliment(result);
+        } else {
+            return result;
+        }
+    }
+
+    public int binToInt(String num) {
+        int result = 0;
+        for (int i = num.length() - 1; i >= 0; i--) {
+            if (num.charAt(i) == 49) {
+                result += pow((i - num.length()), 2);
+            }
+        }
+        return result;
+    }
+}
---
+++ Assembler/Find.java
@@ -0,0 +1,293 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package Assembler;
+
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFileChooser;
+import static java.lang.Math.pow;
+
+public class Find extends javax.swing.JFrame {
+    decoder dec;
+    boolean littleEndian;
+    boolean fillFile;
+    File mips;
+    File directory;
+    final JFileChooser mipsfc = new JFileChooser();
+    String filePath;
+
+    public Find() {
+        initComponents();
+    }
+
+    private void initComponents() {
+
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jCheckBox1 = new javax.swing.JCheckBox();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jCheckBox2 = new javax.swing.JCheckBox();
+        jTextField1 = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("CR16 Assembler");
+
+        jButton1.setText("Select Assembly File");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("Clear");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jCheckBox1.setText("Little Endian");
+        jCheckBox1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jCheckBox1ActionPerformed(evt);
+            }
+        });
+
+        jTextArea1.setEditable(false);
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        jButton3.setText("Generate Binary Machine Code");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
+        jButton4.setText("Generate Hex Machine Code");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
+        jCheckBox2.setText("Pad Machine Code with zeros");
+        jCheckBox2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jCheckBox2ActionPerformed(evt);
+            }
+        });
+
+        jTextField1.setText("16");
+        jTextField1.setEnabled(false);
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("Address Length in Bits");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(layout
+                .createSequentialGroup().addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 395, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false).addGroup(layout
+                        .createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(jCheckBox1)
+                                .addComponent(jCheckBox2, javax.swing.GroupLayout.DEFAULT_SIZE,
+                                        javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE,
+                                        javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE,
+                                        javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE,
+                                        javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE,
+                                        javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                        .addGroup(layout.createSequentialGroup()
+                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 118,
+                                        javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED,
+                                        javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 173,
+                                        javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap()));
+        layout.setVerticalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(layout
+                .createSequentialGroup().addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(layout
+                        .createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                .addComponent(jButton1).addComponent(jCheckBox1))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                .addComponent(jButton3).addComponent(jCheckBox2))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                .addComponent(jButton4).addComponent(jLabel1))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE,
+                                javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 36, Short.MAX_VALUE)
+                        .addComponent(jButton2)).addComponent(jScrollPane1))
+                .addContainerGap()));
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton1ActionPerformed
+        int val = mipsfc.showOpenDialog(Find.this);
+        if (val == mipsfc.APPROVE_OPTION) {
+            mips = mipsfc.getSelectedFile();
+            directory = mipsfc.getCurrentDirectory();
+            jTextArea1.append(mips.getName() + " has been selected\n");
+
+        } else {
+            mipsfc.hide();
+        }
+    }// GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton2ActionPerformed
+        jCheckBox1.setSelected(false);
+        jTextField1.setText("16");
+        jTextField1.setEnabled(false);
+        jCheckBox2.setSelected(false);
+        jTextArea1.setText("The Assembler has been reset.\n");
+
+    }// GEN-LAST:event_jButton2ActionPerformed
+
+    private void jCheckBox1ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jCheckBox1ActionPerformed
+        if (jCheckBox1.isSelected()) {
+            jTextArea1.append("Little Endian has been selected\n");
+        } else {
+            jTextArea1.append("Little Endian has been deselected\n");
+        }
+
+    }// GEN-LAST:event_jCheckBox1ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {
+        try {
+            int numberOfFileLines = Integer.parseInt(jTextField1.getText());
+            jTextArea1.append("The decoding has begun\n");
+            dec = new decoder(mips, directory, jCheckBox1.isSelected(), jCheckBox2.isSelected(), numberOfFileLines);
+            if (dec.printBinary()) {
+                jTextArea1.append("The machine code binary file has successfully created!\n");
+            } else {
+                jTextArea1.append("The creation of the binary machine code file has failed!\n");
+            }
+        } catch (NumberFormatException nfe) {
+            jTextArea1.append("Please input an integer for the \"Address Length in Bits\" text box.\n");
+        } catch (Exception ex) {
+            Logger.getLogger(Find.class.getName()).log(Level.SEVERE, null, ex);
+            jTextArea1.append("The decoding has failed\n");
+        }
+
+    }
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {
+        try {
+            int numberOfFileLines = (int) pow(2, Integer.parseInt(jTextField1.getText()));
+            jTextArea1.append("The Assembling has begun\n");
+            dec = new decoder(mips, directory, jCheckBox1.isSelected(), jCheckBox2.isSelected(), numberOfFileLines);
+            if (dec.printHex()) {
+                jTextArea1.append("The encoded machine code hex file has successfully created!\n");
+            } else {
+                jTextArea1.append("The creation of the hex machine code file has failed!\n");
+            }
+        } catch (NumberFormatException nfe) {
+            jTextArea1.append("Please input an integer for the \"Address Length in Bits\" text box.\n");
+
+        } catch (Exception ex) {
+            Logger.getLogger(Find.class.getName()).log(Level.SEVERE, null, ex);
+            jTextArea1.append("The decoding has failed\n");
+        }
+    }
+
+    private void jCheckBox2ActionPerformed(java.awt.event.ActionEvent evt) {
+        if (jCheckBox2.isSelected()) {
+            fillFile = true;
+            jTextField1.setEnabled(true);
+            jTextArea1.append("The remainder of the file will be filled with 0's.\n");
+        } else {
+            jTextField1.enableInputMethods(false);
+            fillFile = false;
+            jTextField1.setEnabled(false);
+            jTextArea1.append("The remainder of the file will not be filled with 0's.\n");
+        }
+    }
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        // <editor-fold defaultstate="collapsed" desc=" Look and feel setting code
+        // (optional) ">
+        /*
+         * If Nimbus (introduced in Java SE 6) is not available, stay with the default
+         * look and feel. For details see
+         * http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
+         */
+        try {
+            javax.swing.UIManager.LookAndFeelInfo[] installedLookAndFeels = javax.swing.UIManager
+                    .getInstalledLookAndFeels();
+            for (int idx = 0; idx < installedLookAndFeels.length; idx++)
+                if ("Nimbus".equals(installedLookAndFeels[idx].getName())) {
+                    javax.swing.UIManager.setLookAndFeel(installedLookAndFeels[idx].getClassName());
+                    break;
+                }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Find.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Find.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Find.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Find.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Find().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JCheckBox jCheckBox1;
+    private javax.swing.JCheckBox jCheckBox2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextField jTextField1;
+}
